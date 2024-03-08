ef5108cea12fd13f41d7f376a6ea5ca64a56175e
michael <michaelslack42@gmail.com>
2024-03-08 13:22:23 +0000

    more tweaks

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,12 +9,12 @@ export default async function LoginPage() {
     <>
       <section className="bg-ct-blue-600 min-h-screen pt-20">
         <div className="container mx-auto px-6 py-12 h-full flex flex-wrap justify-center items-center">
-          <div className="md:w-8/12 lg:w-5/12 bg-white px-8 py-10 border-2 border-solid border-teal-800 rounded-lg">
+          <div className="md:w-8/12  bg-white px-8 py-10 border-2 border-solid border-teal-800 rounded-lg">
             <Suspense fallback={<>Loading...</>}>
               <LoginForm />
             </Suspense>
           </div>
-          <div className="md:w-8/12 lg:w-5/12 bg-white px-8 py-10">
+          <div className="md:w-8/12  bg-white px-8 py-10">
             <Link href={ApplicationRoutes.Register}>
               <CustomButton
                 type="button"
