9249a94bb565531e674bbb3cbe5e35efad2ed35b
michael <michaelslack42@gmail.com>
2024-03-10 21:45:21 +0000

    experimental logging

--- src/app/api/game/changeRotationStatus/route.ts
+++ src/app/api/game/changeRotationStatus/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const { id, newStatus } = await req.json();
+  console.log(`Game rotation status: ${id} ${newStatus}`)
 
   if (!id || newStatus === undefined) {
     return NextResponse.json(
