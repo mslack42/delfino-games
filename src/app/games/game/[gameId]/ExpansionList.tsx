c2f732eb7a51ff5b9477c76004fe16d02e002ab7
michael <michaelslack42@gmail.com>
2024-03-19 11:01:41 +0000

    css tweaks

--- src/app/games/game/[gameId]/ExpansionList.tsx
+++ src/app/games/game/[gameId]/ExpansionList.tsx
@@ -12,7 +12,7 @@ export function ExpansionList({ data }: ExpansionProps) {
       <div className="p-4 pt-2 pb-2 m-4 bg-card rounded-lg max-w-full">
         <div>
           <h1 className="text-2xl font-bold pt-2">With Expansions:</h1>
-          <div className="max-w-full max-h-96 flex flex-row justify-center flex-wrap overflow-hidden hover:overflow-scroll">
+          <div className="min-w-full max-h-96 flex flex-row justify-center flex-wrap overflow-hidden hover:overflow-y-scroll bg-cardScroller rounded-xl">
             {expansions!.map((ex) => (
               <ExpansionSummaryCard data={ex} key={ex.bggId} selected={true} />
             ))}
