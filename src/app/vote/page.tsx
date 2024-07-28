537a8a0127786a3ec979ca7a1094d58d7d872d05
michael <michaelslack42@gmail.com>
2024-07-28 17:08:04 +0100

    actual fix

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -1,10 +1,18 @@
 import { getGamesSuggestions } from "@/database/game-suggestions/getGamesSuggestions";
 import { VoteContent } from "./VoteContent";
 import { getGamesSuggestionVotes } from "@/database/game-suggestion-votes/getGamesSuggestionVotes";
+import { isRole } from "@/util/auth/server/isRole";
 
 export default async function ListGameSuggestions() {
   const gameSuggestions = await getGamesSuggestions();
   const gameVotes = await getGamesSuggestionVotes();
+  const displayVotes = await isRole("Admin");
 
-  return <VoteContent gameSuggestions={gameSuggestions} votes={gameVotes} />;
+  return (
+    <VoteContent
+      gameSuggestions={gameSuggestions}
+      votes={gameVotes}
+      displayVotes={displayVotes}
+    />
+  );
 }
